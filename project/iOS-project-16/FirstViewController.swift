12ebbe9f66f4c4bdc0601826aee164668bfedc3d
macbook_user <macbook_user@CS-M100.local>
2016-10-27 00:15:47 -0500

    Adding inital files to the project

---
+++ project/iOS-project-16/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOS-project-16
+//
+//  Created by macbook_user on 10/27/16.
+//  Copyright © 2016 Kaustubh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
