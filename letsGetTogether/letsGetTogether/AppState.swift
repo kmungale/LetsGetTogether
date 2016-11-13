1e535d9ff0ee948c8ec29846cfab4ed0b1b4ddec
macbook_user <macbook_user@cs-m102.home>
2016-11-13 01:45:07 -0600

    Implemented the one time login and logout functionality

---
+++ letsGetTogether/letsGetTogether/AppState.swift
@@ -0,0 +1,13 @@
+//
+//  AppState.swift
+//  letsGetTogether
+//
+//  Created by macbook_user on 11/12/16.
+//  Copyright © 2016 Kaustubh. All rights reserved.
+//
+
+import UIKit
+
+class AppState: NSObject {
+
+}
